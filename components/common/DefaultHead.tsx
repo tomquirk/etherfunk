7c20265cf8e9e0f448e8bc8a35bafe4a1759bf79
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-16 14:40:38 +0930

    Add some meta

--- components/common/DefaultHead.tsx
+++ components/common/DefaultHead.tsx
@@ -21,14 +21,27 @@ export function DefaultHead() {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
+      <link rel="icon" href="/favicon.ico" />
       <meta name="msapplication-TileColor" content="#013076" />
       <meta name="theme-color" content="#ffffff"></meta>
-
       <meta
         name="description"
-        content="Your Ethereum Control Panel. Interact with smart contracts on Ethereum."
+        content="Your Ethereum control panel. Interact with smart contracts on Ethereum."
       />
-      <link rel="icon" href="/favicon.ico" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://etherfunk.io" />
+      <meta
+        property="og:image"
+        content="https://etherfunk.io/android-chrome-256x256.png"
+      />
+      <meta
+        property="og:title"
+        content="Etherfunk | Your Ethereum control panel"
+      />
+      <meta
+        name="og:description"
+        content="Interact with smart contracts on Ethereum. Your Ethereum control panel."
+      />
       <script
         src="https://tvc15-essential.etherfunk.io/script.js"
         data-site="JIKXTFHK"
