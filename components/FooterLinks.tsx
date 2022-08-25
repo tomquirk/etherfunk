23579735846af58bd9717fe54159434a67eb2a86
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-26 07:41:23 +0930

    Add tg link

--- components/FooterLinks.tsx
+++ components/FooterLinks.tsx
@@ -18,6 +18,15 @@ export function FooterLinks() {
       >
         Fund
       </a>
+      <span className="mx-2 text-slate-400 font-light">|</span>
+      <a
+        href="https://t.me/etherfunk"
+        className="text-sm hover:underline text-blue-600 hover:text-blue-800 visited:text-purple-800"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Telegram
+      </a>
     </div>
   );
 }
